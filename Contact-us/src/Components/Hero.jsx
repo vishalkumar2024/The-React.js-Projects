55b80aa4172f8cac09019aeff241554471ef2769
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-03-18 16:50:35 +0530

    do not know why

--- Contact-us/src/Components/Hero.jsx
+++ Contact-us/src/Components/Hero.jsx
@@ -3,16 +3,27 @@ import { MdOutlineMessage } from "react-icons/md";
 import { IoMdCall } from "react-icons/io";
 
 export default function Hero() {
+    function firstBtnStyle() {
+        alert("Do you want support chat?");
+    }
+    function secondBtnStyle() {
+        alert("I am making a call.");
+    }
+    function onsubmit(e){
+        e.preventDefault();
+        console.log("e")
+    }
+
     return (
         <div className='flex mt-6 max-md:flex-col'>
             {/* Left Div part */}
             <div className='w-1/2 max-md:w-full'>
                 <div className='flex items-center justify-between'>
-                    <button className='flex text-[1.05rem] px-[5%] py-[5%] mr-0.5 bg-black text-white uppercase rounded '> <span className='mr-3 mt-[5px] '><MdOutlineMessage /></span>  <p className=' max-lg:text-[0.7rem] max-md:text-[1rem]   max-[430px]:text-6xl'> View support chat
+                    <button onClick={firstBtnStyle} className='flex text-[1.05rem] px-[5%] py-[5%] mr-0.5 bg-black text-white uppercase rounded '> <span className='mr-3 mt-[5px] '><MdOutlineMessage /></span>  <p className=' max-lg:text-[0.7rem] max-md:text-[1rem]   max-[430px]:text-6xl'> View support chat
                     </p>
                     </button>
 
-                    <button className='flex text-[1.05rem] px-[15%] py-[5%] bg-black text-white uppercase rounded'> <span className='mr-3 mt-[5px]'><IoMdCall /></span>  <p className=' max-lg:text-[0.7rem] max-md:text-[1rem]'> Via call
+                    <button  onClick={secondBtnStyle} className='flex text-[1.05rem] px-[15%] py-[5%] bg-black text-white uppercase rounded'> <span className='mr-3 mt-[5px]'><IoMdCall /></span>  <p className=' max-lg:text-[0.7rem] max-md:text-[1rem]'> Via call
                     </p>
                     </button>
                 </div>
@@ -20,7 +31,8 @@ export default function Hero() {
                 <button className='flex w-full border-2 border-black mt-10   text-[1.05rem] px-[40%]  text-black uppercase rounded py-[2%] '> <span className='mr-3 mt-[5px]'><IoMdCall /></span>  <p className=' max-lg:text-[0.8rem] max-md:text-[1rem]'> Via email form
                 </p>
                 </button>
-                <form className='mt-4'>
+
+                <form onSubmit={onsubmit} className='mt-4'>
                     <div className='flex flex-col mt-8 relative'>
                         <label htmlFor="Name" className='bg-white px-1 font-semibold absolute left-5 -top-[14px]'>Name</label>
                         <input type="text" className='w-full h-[40px] pl-4 py-3 border border-gray-500 rounded max-md:h-[60px]' />
