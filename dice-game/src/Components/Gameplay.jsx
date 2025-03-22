0e693ac95e4d1fe6db1f12a1c493d876e12d2b93
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-03-22 22:19:28 +0530

    modified

--- dice-game/src/Components/Gameplay.jsx
+++ dice-game/src/Components/Gameplay.jsx
@@ -3,8 +3,12 @@ import React, { useState } from 'react'
 export default function Gameplay() {
     let diceNumber = [1, 2, 3, 4, 5, 6];
     const [selected, setSelected] = useState();
-    console.log(selected)
-    // let isSelected=false;
+    console.log("selected number in button is"+selected)
+
+    let rollDice=()=>{
+        return Math.floor(Math.random() * 6) + 1
+    }
+  console.log(`rolled dice number is ${rollDice()}`)
 
     return (
         <div className='p-10'>
@@ -17,19 +21,21 @@ export default function Gameplay() {
                     <div className=' w-full flex justify-evenly'>
                         {
                             diceNumber.map((value, i) => (
-                                <button onClick={() => setSelected(value)} isSelected={value===selected} className='px-4 py-[10px] border-2 border-black ${isSelected?bg-black:bg-white} font-semibold hover:bg-black hover:text-white' key={i}>{value}</button>
+                                <button onClick={() => setSelected(value)} className={`px-4 py-[10px] border-2 border-black   ${selected === value ? 'bg-black text-white' : 'bg-white'} font-semibold hover:bg-black hover:text-white`} key={i}>{value}</button>
                             ))
                         }
                     </div>
                     <p className='capitalize font-semibold text-[1.2rem] mt-3'>select number</p>
                 </div>
             </div>
-            <div className='bg-rose-300 w-full h-[430px] flex justify-center items-center'>
-                <div className='bg-amber-300 w-[15%] h-[80%] flex '>
-                    <div></div>
-                    <p>Click on Dice to roll</p>
-                    <button className=''>reset score</button>
-                    <button className='bg-red-400'>show rules</button>
+            <div className=' w-full h-[430px] flex justify-center items-center'>
+                <div className=' w-[15%] h-[80%] flex flex-col '>
+                    <div  className=' w-[90%] h-[150px] m-2 '>
+                        <img  className='size-full' src={`/public/Images/dice_${rollDice()}.png`} alt="dice" />
+                    </div>
+                    <p onClick={()=>rollDice()} className=' m-5 font-semibold text-[1.05rem]'>Click on Dice to roll</p>
+                    <button className='btn-style mx-4 px-5 font-semibold bg-white text-black border border-black py-2 mt-3 hover:bg-black hover:text-white'>reset score</button>
+                    <button className='btn-style mx-4 px-5 mt-10'>show rules</button>
                 </div>
             </div>
         </div>
