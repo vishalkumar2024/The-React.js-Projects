7206d609ca295d9cda32de81862e0c597567ef61
vishalkumar2024 <vkumarsoni30@gmail.com>
2025-12-30 12:57:22 +0530

    feat: update Hero and Navbar components for improved styling and mobile menu functionality in e commerce

--- E-commerce/src/Components/Hero.jsx
+++ E-commerce/src/Components/Hero.jsx
@@ -8,23 +8,23 @@ import './Hero.css'
 
 function Hero() {
     return (
-        <div className='flex mt-[2px] mb-10 pt-36 bg-linear-to-b from-[#f6cbf9] to-[#e1ffea22]' id="HeroMain">
+        <div className='flex mb-10 pt-22 bg-linear-to-b from-[#adf8ec] to-[#e1ffea22]  max-md:pt-0' id="HeroMain">
 
             {/* Left Part */}
-            <div className=' h-full w-1/2   flex items-center justify-center  pl-[100px] '>
-                <div className='w-[70%]' >
+            <div className=' h-full w-[50%]   flex items-center justify-center  pl-[100px] max-lg:pl-[40px] max-md:w-[100%]'>
+                <div className='w-[70%]  max-md:w-[80%]' >
                     <h2 className='text-[#090909] text-[26px] uppercase font-semibold'>New Arrival Only</h2>
 
                     <div className=''>
-                        <div className='flex '>
-                            <p className='text-8xl font-semibold mb-2'>new</p>
-                            <span>  <img className='w-[100px] ' src={handIcon} alt="" /> </span>
+                        <div className='flex 0 '>
+                            <p className='text-8xl  font-semibold mb-2 max-lg:text-7xl max-md:text-[120px]'>new</p>
+                            <span className=' max-md:pt-8'>  <img className='w-[100px] max-lg:w-[70px] ' src={handIcon} alt="" /> </span>
                         </div>
-                        <p className='text-7xl font-semibold mb-1'>collections</p>
-                        <p className='text-[65px] font-bold mb-1'>for everyone</p>
+                        <p className='text-7xl font-semibold mb-1 max-lg:text-6xl'>collections</p>
+                        <p className='text-[65px] font-bold mb-1 max-lg:text-6xl'>for everyone</p>
                     </div>
 
-                    <div className='flex items-center justify-center gap-4 cursor-pointer w-[250px] h-[40px] rounded-4xl mt-8 bg-red-600 text-white text-[18px] font-semibold'>
+                    <div className='flex items-center justify-center gap-4 cursor-pointer w-[250px] h-[40px] rounded-4xl mt-8 bg-red-600 text-white text-[18px] font-semibold max-lg:w-[220px] max-lg:h-[30px] '>
                         <div>Latest Collection</div>
                         <img src={arrowIcon} alt="" />
                     </div>
@@ -33,8 +33,8 @@ function Hero() {
             </div>
 
             {/* Right Part */}
-            <div className='w-1/2 '>
-                <img className=' h-full pl-25' src={heroImage} alt="" />
+            <div className='w-[50%]  max-md:w-[100%] max-md:hidden  '>
+                <img className=' h-full pl-25 max-lg:h-[90%] max-lg:pl-20 ' src={heroImage} alt="" />
             </div>
         </div>
     )
