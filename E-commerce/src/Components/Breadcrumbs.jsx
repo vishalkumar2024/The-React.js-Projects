04c6eb0db7794bcff1bf0089921af129280d1b2f
vishalkumar2024 <vkumarsoni30@gmail.com>
2026-01-09 14:10:36 +0530

    improve layout and styling of Breadcrumbs components for better responsiveness in E commerce

--- E-commerce/src/Components/Breadcrumbs.jsx
+++ E-commerce/src/Components/Breadcrumbs.jsx
@@ -6,8 +6,8 @@ function Breadcrumbs(props) {
   const { product } = props;
 
   return (
-    <div className='flex items-center text-[#5e5e5e] gap-4 text-4 font-semibold mt-[30px] mb-5 ml-[150px] capitalize max-lg:text-[15px] '>
-      <span className='cursor-pointer hover:text-blue-500 '>HOME</span> <img src={breadcrumArrowIcon} alt="" /> <span  className='cursor-pointer hover:text-blue-500 '>SHOP</span> <img src={breadcrumArrowIcon} alt="" /><span  className='cursor-pointer hover:text-blue-500 '> {product.category} </span><img src={breadcrumArrowIcon} alt="" /><span  className='cursor-pointer hover:text-blue-500 '> {product.name} </span>
+    <div className='flex items-center text-[#5e5e5e] gap-4 text-4 font-semibold mt-[30px] mb-5 ml-[150px] capitalize max-lg:text-[15px] max-lg:ml-[50px] max-md:ml-[20px]  max-md:text-[13px]  max-md:gap-2 '>
+      <span className='cursor-pointer hover:text-blue-500 '>HOME</span> <img src={breadcrumArrowIcon} alt="" className='max-md:h-[10px] ' /> <span  className='cursor-pointer hover:text-blue-500 '>SHOP</span> <img src={breadcrumArrowIcon} alt="" className='max-md:h-[10px] ' /><span  className='cursor-pointer hover:text-blue-500 '> {product.category} </span><img src={breadcrumArrowIcon} alt="" className='max-md:h-[10px] ' /><span  className='cursor-pointer hover:text-blue-500 '> {product.name} </span>
     </div>
   )
 }
--- E-commerce/src/Components/RelatedProducts.jsx
+++ E-commerce/src/Components/RelatedProducts.jsx
@@ -6,7 +6,7 @@ import Item from '../Components/Item'
 function RelatedProducts(props) {
     const { product } = props
     return (
-        <div className='flex bg-green-300 flex-col items-center gap-3 pb-[50px] mt-12 mb-5 '>
+        <div className='flex  flex-col items-center gap-3 pb-[50px] mt-12 mb-5 '>
             <h1 className='text-[#171717] text-[50px] font-stretch-semi-condensed max-sm:text-[32px] max-lg:text-[30px]'>Related Products</h1>
             <hr className='w-[200px] h-[6px] rounded bg-[#252525]  max-lg:w-[130px] max-md:w-[100px] max-md:h-[3px]  ' />
             <div id="relatedProduct-item" className='flex gap-7 mt-12 max-lg:gap-3 max-md:grid max-md:grid-cols-2'>
